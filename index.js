a74948bdbbe1809201f4b7c6b996e523738e6fae
TanNguyen <tannmps09830@fpt.edu.vn>
2020-09-24 21:53:06 +0700

    Tan
    Thêm bottom navigation

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import Home from './src/components/screens/Home'
-AppRegistry.registerComponent(appName, () => Home);
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
