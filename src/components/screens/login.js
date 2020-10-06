2219299cf03013dee29b38529b69f2e2f6b9b911
baalzbarbarian <nguyenduchai.19021992@gmail.com>
2020-10-06 15:52:54 +0700

    Update Login with Google
    Nguyen Hai

--- src/components/screens/login.js
+++ src/components/screens/login.js
@@ -11,6 +11,7 @@ import {
 import Icons from 'react-native-vector-icons/EvilIcons';
 import {AccessToken, LoginManager} from 'react-native-fbsdk';
 import {LoginGoogle} from '../../Redux/actions/userAction';
+import {GoogleSignin, statusCodes} from '@react-native-community/google-signin';
 import {useDispatch, useSelector} from 'react-redux';
 import {Fonts} from '../../utils/Fonts';
 
@@ -19,6 +20,15 @@ const IMAGE = {
   uri:
     'https://firebasestorage.googleapis.com/v0/b/geapp-d5a80.appspot.com/o/kevin-mueller-0ytwNH74s3A-unsplash.jpg?alt=media&token=14251aed-7d13-44af-a015-929e4d0d4144',
 };
+
+GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+GoogleSignin.configure({
+  webClientId:
+    '835610037209-t6p9r8dedt7e7m8hh302br39es9ctfe7.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+  offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  hostedDomain: '', // specifies a hosted domain restriction
+  forceCodeForRefreshToken: true, // [Android] related to `serverAuthCode`, read the docs link below *.
+});
 
 export default function Login() {
   const dispatch = useDispatch();
@@ -61,6 +71,55 @@ export default function Login() {
     );
   };
 
+  const onLoginGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log(userInfo);
+
+      // if (LoginGoogle) {
+      //   let dataUser = {
+      //     Name: LoginGoogle.data.FirstName + LoginGoogle.data.LastName,
+      //     userName: LoginGoogle.data.UserName,
+      //     email: LoginGoogle.data.Email,
+      //     id: LoginGoogle.data.Id,
+      //     avatar: userInfo.user.photo,
+      //   };
+
+      //await AsyncStorage.setItem('token', userInfo.idToken);
+      //await AsyncStorage.setItem('user', JSON.stringify(dataUser));
+
+      // dispatch(
+      //   loginAction({
+      //     token: userInfo.idToken,
+      //     logged: true,
+      //     user: dataUser,
+      //   }),
+      // );
+      // navigation.navigate('BottomTab');
+
+      //   console.log(dataUser);
+      // } else {
+      //   GoogleSignin.signOut();
+      // }
+    } catch (error) {
+      console.log(error);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('oke SIGN_IN_CANCELLED');
+        GoogleSignin.signOut();
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('oke IN_PROGRESS');
+        GoogleSignin.signOut();
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('oke PLAY_SERVICES_NOT_AVAILABLE');
+        GoogleSignin.signOut();
+      } else {
+        console.log('oke oke');
+        GoogleSignin.signOut();
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground source={IMAGE} style={styles.imageBackground}>
@@ -81,7 +140,7 @@ export default function Login() {
 
           <View style={{height: 15}} />
 
-          <TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => onLoginGoogle()}>
             <View style={styles.buttonGG}>
               <Icons name="sc-google-plus" size={24} color="#fff" />
               <View style={{width: 10}} />
