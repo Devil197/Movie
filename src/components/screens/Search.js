644d0355d662c3e5a24a36ed1b226535ab89f888
baalzbarbarian <nguyenduchai.19021992@gmail.com>
2020-10-20 22:47:59 +0700

    NguyenHai
    Search SS1

---
+++ src/components/screens/Search.js
@@ -0,0 +1,205 @@
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TextInput,
+  Animated,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+import EvilIcon from 'react-native-vector-icons/EvilIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {HEIGHT, WIDTH} from '../../constants/constants';
+import ActorsList from '../views/listCustom/lists/actorList';
+import MoviesList from '../views/listCustom/lists/movieList';
+import {Fonts} from '../../utils/Fonts';
+import {MySpinner} from '../views';
+import SearchInput, {createFilter} from 'react-native-search-filter';
+import {SkypeIndicator} from 'react-native-indicators';
+import KeyWords from '../views/searchComponent';
+import actorDataTest from '../../constants/data/cast';
+import {films} from '../../constants/data/fakeData';
+import AsyncStorage from '@react-native-community/async-storage';
+
+const STATUS_BAR_CURRENT_HEIGHT =
+  Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+const HEADER_HEIGHT = 50;
+const KEYWORDS = 'keywords';
+
+export default function Search({navigation}) {
+  const scrollY = new Animated.Value(0);
+  const diffClamp = Animated.diffClamp(scrollY, 0, HEADER_HEIGHT);
+
+  const translateY = diffClamp.interpolate({
+    inputRange: [0, HEADER_HEIGHT],
+    outputRange: [0, -HEADER_HEIGHT],
+  });
+
+  const [keyword, setKeyword] = useState('');
+  const [listKey, setListKey] = useState([]);
+  useEffect(() => {
+    getKey();
+  }, []);
+
+  const getKey = async () => {
+    //await AsyncStorage.removeItem(KEYWORDS)
+    try {
+      let string = await AsyncStorage.getItem(KEYWORDS);
+      let obj = await JSON.parse(string);
+      console.log("OBJ ", obj);
+
+      setListKey(obj);
+      console.log('LIST KEY Search.js ', listKey);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleAddNewKeyword = async () => {
+    if (listKey != null) {
+      let index = listKey.indexOf(keyword);
+      console.log('IndexOf ' + keyword + ' = ' + index);
+
+      if (index > -1) {
+        listKey.splice(index, 1);
+        setListKey([...listKey], keyword);
+        try {
+          await AsyncStorage.removeItem(KEYWORDS).then(() => {
+            console.log('Keyword removed');
+          });
+          AsyncStorage.setItem(KEYWORDS, JSON.stringify(listKey)).then(() => {
+            console.log('KEYWORDS updated.');
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+
+    try {
+      let keywords = (await AsyncStorage.getItem(KEYWORDS)) || '[]';
+      keywords = JSON.parse(keywords);
+      keywords.push(keyword);
+      AsyncStorage.setItem(KEYWORDS, JSON.stringify(keywords)).then(() => {
+        console.log('KEYWORDS added.');
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* <StatusBar backgroundColor="rgba(0, 0, 0, 0.20)" /> */}
+      <Animated.View
+        style={{
+          transform: [{translateY: translateY}],
+          zIndex: 10,
+        }}>
+        <View style={styles.searchBar}>
+          <View style={styles.backIcon}>
+            <FontAwesome5 name="arrow-left" size={18} color={'black'} />
+          </View>
+          <View style={styles.searchInputContainer}>
+            <EvilIcon name="search" size={22} color={'gray'} />
+            <TextInput
+              autoFocus={true}
+              value={keyword}
+              onChangeText={(txt) => setKeyword(txt)}
+              style={styles.searchInput}
+              placeholder={'Tìm kiếm với GEA'}
+              placeholderTextColor={'gray'}
+              onSubmitEditing={handleAddNewKeyword}
+              returnKeyType="search"
+            />
+          </View>
+        </View>
+      </Animated.View>
+
+      {/* <View style={styles.contentContainer}> */}
+      {/* Actors */}
+      {/* <View style={styles.actorContainer}>
+          <Text style={styles.title}>Actors</Text>
+          <ActorsList data={actorDataTest} />
+        </View> */}
+
+      {/* Movies */}
+      {/* <View style={styles.movieContainer}>
+            <Text style={styles.title}>Movies</Text>
+            <MoviesList data={films} />
+        </View> */}
+      {/* </View> */}
+
+      <View style={styles.contentContainer}>
+        <ScrollView contentContainerStyle={styles.keyWordContainer}>
+          <KeyWords />
+        </ScrollView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: STATUS_BAR_CURRENT_HEIGHT,
+    flex: 1,
+  },
+  searchBar: {
+    height: HEADER_HEIGHT,
+    flexDirection: 'row',
+    borderBottomWidth: 0.1,
+    borderBottomColor: 'rgba(166, 164, 164, 0.1)',
+    alignItems: 'center',
+  },
+  backIcon: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  searchInputContainer: {
+    flex: 5,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: 'rgba(166, 164, 164, 0.3)',
+    height: 40,
+    paddingLeft: 25,
+    marginRight: 10,
+    borderRadius: 20,
+  },
+  searchInput: {
+    width: '85%',
+    color: 'gray',
+    fontSize: 16,
+    height: 41,
+    paddingLeft: 5,
+  },
+  contentContainer: {
+    height: '100%',
+    backgroundColor: '#fff',
+    //backgroundColor: 'rgba(166, 164, 164, 0.3)',
+  },
+  keyWordContainer: {
+    flex: 1,
+  },
+  // actorContainer: {
+  //   height: HEIGHT * 0.25,
+  //   backgroundColor: '#fff',
+  //   borderRadius: 10,
+  // },
+  // title: {
+  //   fontFamily: Fonts.SansMedium,
+  //   fontSize: 18,
+  //   paddingLeft: 10,
+  //   paddingTop: 10,
+  //   color: 'rgba(0, 0, 0, 0.40)',
+  // },
+  // movieContainer: {
+  //   marginTop: 10,
+  //   borderRadius: 10,
+  //   backgroundColor: '#fff',
+  // },
+});
---
+++ src/components/views/searchComponent.js
@@ -0,0 +1,79 @@
+import React, {useEffect, useState} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {Fonts} from '../../utils/Fonts';
+import AsyncStorage from '@react-native-community/async-storage';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+const KEYWORDS = 'keywords';
+export default function searchComponent() {
+  const [listKey, setListKey] = useState([]);
+
+  useEffect(() => {
+    getKeyword();
+    console.log('Keyword Searched! = ',listKey);
+  }, []);
+
+  const getKeyword = async () => {
+    let stringify = await AsyncStorage.getItem(KEYWORDS);
+    let obj = JSON.parse(stringify);
+    setListKey(obj);
+    console.log('Keyword Searched! = ', listKey);
+  };
+
+  if (listKey == []) {
+    return (
+      <View style={styles.withoutKeywords}>
+        <FontAwesome5
+          name="search"
+          size={30}
+          color={'rgba(166, 164, 164, 0.8)'}
+        />
+        <Text style={styles.txtWithoutKeywords}>
+          Hãy nhập từ khóa để tìm kiếm với GEA
+        </Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.oldKeyTitleContainer}>
+        <Text style={styles.oldKeyTitle}>KEYWORD SEARCHED</Text>
+      </View>
+      {listKey.map((value) => {
+        <View style={{backgroundColor: 'green', width: '100%', height: 200}}>
+          <Text style={{color: '#000'}}>{value}</Text>
+        </View>;
+      })}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  oldKeyTitleContainer: {
+    height: 30,
+    borderBottomColor: 'rgba(0, 0, 0, 0.2 )',
+    borderBottomWidth: 0.5,
+    width: '100%',
+    justifyContent: 'center',
+  },
+  oldKeyTitle: {
+    fontFamily: Fonts.SansMedium,
+    fontSize: 16,
+    paddingLeft: 10,
+  },
+  withoutKeywords: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: '50%',
+  },
+  txtWithoutKeywords: {
+    fontFamily: Fonts.SansLight,
+    color: 'rgba(166, 164, 164, 0.8)',
+    fontSize: 16,
+    marginTop: 10,
+  },
+});
