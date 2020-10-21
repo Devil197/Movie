c33034e918fb0aaa6444e153b6a2def0948c48f9
locminhtruongps09754 <locminhtruongps09754@gmail.com>
2020-10-21 20:33:58 +0700

    redux keyword

--- src/Redux/reducers/index.js
+++ src/Redux/reducers/index.js
@@ -3,6 +3,7 @@ import {combineReducers} from 'redux';
 import {persistReducer} from 'redux-persist';
 // import
 import userReducer from './userReducer';
+import keywordReducer from './keywordReducer'
 
 const reducers = combineReducers({
   userReducer: persistReducer(
@@ -11,6 +12,13 @@ const reducers = combineReducers({
       storage: AsyncStorage,
     },
     userReducer,
+  ),
+  keywordReducer: persistReducer(
+    {
+      key: 'keywordReducer',
+      storage: AsyncStorage,
+    },
+    keywordReducer,
   ),
 });
 // Exports
