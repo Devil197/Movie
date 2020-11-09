0badcf7c7b490e88dddebdd43c5d05b072788171
locminhtruongps09754 <locminhtruongps09754@gmail.com>
2020-11-09 20:50:03 +0700

    update service

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ import {
 //import App from './src/App';
 import App from './src/App';
 import messaging from '@react-native-firebase/messaging';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import 'react-native-gesture-handler';
 import PushNotification from 'react-native-push-notification';
+import SomeTaskName from './src/Headless/SomeTaskName'
 LogBox.ignoreAllLogs(true);
 
 const channel = {
@@ -26,30 +27,11 @@ const channel = {
   description: 'movie mobile app description',
 };
 
-messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-  console.log('1001 -> noti ne` : ', remoteMessage);
-  ToastAndroid.show(
-    'chay setBackgroundMessageHandler NE NHA' + remoteMessage &&
-      remoteMessage.notification &&
-      Platform.OS === 'android'
-      ? remoteMessage.notification.title
-      : '',
-    ToastAndroid.SHORT,
-  );
-
-  PushNotification.localNotification({
-    title: remoteMessage.data.movie_name,
-    message: 'abc',
-    channelId: 'movie0103',
-    id: moment().seconds(),
-    largeIconUrl: remoteMessage.data.photo,
-  });
-});
-
 if (Platform.OS === 'android') {
   StatusBar.setTranslucent(true);
   StatusBar.setBackgroundColor('transparent');
 }
 StatusBar.setBarStyle('dark-content');
 // Orientation.lockToPortrait();
+AppRegistry.registerHeadlessTask('SomeTaskName', () => SomeTaskName);
 AppRegistry.registerComponent(appName, () => App);
